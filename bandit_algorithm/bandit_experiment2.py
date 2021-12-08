5c04ba69290f8997b60160694ecc1a6d42e13ba0
Lizz <60306066+mewmiyu@users.noreply.github.com>
2021-12-08 17:46:30 +0100

    Update bandit_experiment2.py

--- bandit_algorithm/bandit_experiment2.py
+++ bandit_algorithm/bandit_experiment2.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
     action_values = np.random.uniform(low=-10, high=0, size=(k,))
     for alpha in ([0.01, .1, 1, 10]):
         bdt = Bandit2(k, alpha, action_values)
-        bdt2 = Bandit(k, alpha, action_values)
+        bdt2 = Bandit(k, alpha, action_values, True)
         bdt.play(1000)
         bdt2.play(1000)
         avg_best_reward = bdt2.best_avg_reward
         avg_reward1 = bdt.avg_reward
         actual_q = action_values
-        policy = bdt.policy
+        policy = bdt.log_policy
 
         estimated_q = [np.exp(alpha * actual_q[x]) * policy[x] for x in range(len(actual_q))]
         estimated_q = [np.log((estimated_q[i]) / sum(estimated_q)) for i in range(len(estimated_q))]
