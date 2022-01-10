ecfc53f0a517cc32bb78c7129879cd807a497cc7
Nyanyan <60306066+mewmiyu@users.noreply.github.com>
2022-01-10 15:38:25 +0100

    Create experiment.py
    Simple experiment for G-Learning

---
+++ model-free_algorithms/g-learning/experiment.py
@@ -0,0 +1,54 @@
+import numpy as np
+from mushroom_rl.environments import GridWorld
+
+"""
+Source: https://mushroomrl.readthedocs.io/en/latest/source/tutorials/tutorials.0_experiments.html
+"""
+
+if __name__ == '__main__':
+    from mushroom_rl.core import Core
+    from g_learning import GLearning
+    from mushroom_rl.algorithms.value.td.q_learning import QLearning
+    from mushroom_rl.policy import EpsGreedy
+    from mushroom_rl.utils.parameters import Parameter
+    from mushroom_rl.utils.dataset import compute_J
+
+    # Set the seed
+    np.random.seed(1)
+
+    # Create the grid environment
+    env = GridWorld(height=5, width=5, start=(0, 0), goal=(2, 2))
+    # Using an epsilon-greedy policy
+    epsilon = Parameter(value=0.1)
+    pi = EpsGreedy(epsilon=epsilon)
+
+    env.reset()
+    env.render()
+
+    learning_rate = Parameter(.1 / 10)
+
+    approximator_params = dict(input_shape=10,
+                               output_shape=(env.info.action_space.n,),
+                               n_actions=env.info.action_space.n)
+
+    agent = GLearning(env.info, pi, learning_rate=learning_rate)
+    print(env.info)
+    # Reinforcement learning experiment
+    core = Core(agent, env)
+
+    # Visualize initial policy for 3 episodes
+    dataset = core.evaluate(n_episodes=3, render=True)
+
+    # Print the average objective value before learning
+    J = np.mean(compute_J(dataset, env.info.gamma))
+    print(f'Objective function before learning: {J}')
+
+    # Train
+    core.learn(n_steps=2000, n_steps_per_fit=1, render=False)
+
+    # Visualize results for 3 episodes
+    dataset = core.evaluate(n_episodes=3, render=True)
+
+    # Print the average objective value after learning
+    J = np.mean(compute_J(dataset, env.info.gamma))
+    print(f'Objective function after learning: {J}')
